656aebb588468a51ef25c325e572dac844b5a2b9
Ravi Kumar Pasumarthy <ravi.pasumarthy@gmail.com>
2014-07-13 23:10:45 +0530

    Fixed bug, instead of using global active power, global reactive power is used

--- plot2.R
+++ plot2.R
@@ -11,7 +11,7 @@ if(!file.exists(subsetfile))
 
 dataset <- read.csv(subsetfile, header=TRUE)
 png(filename="./output/plot2.png", width=480, height=480)
-plot(as.POSIXlt(dataset$DateTime), dataset$Global_reactive_power, type="l", 
+plot(as.POSIXlt(dataset$DateTime), dataset$Global_active_power, type="l", 
      xlab="",
      ylab="Global Active Power (kilowatts)")
 dev.off()
